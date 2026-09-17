2fbbf0b062f8574c4d1a11b1a1f573ee3c5eb0ed
hassanhabib111978-ux <hassanhabib111978@gmail.com>
2026-09-17 11:54:24 +0300

    chore: refresh service worker cache for worker save message v3

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "madkhal-v20";
+const CACHE_NAME = "madkhal-v21";
 
 const BASE_URL = new URL("./", self.registration.scope);
 const INDEX_URL = new URL("index.html", BASE_URL).href;
